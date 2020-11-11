23bc3d05e8cf7d2f2733076414dad2e2502a8efc
Ryan Jacobson <ryanthejacobson@gmail.com>
2020-11-11 15:17:58 -0600

    yelp

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning up Database'
+Restaurant.destroy_all
+puts 'Database is clean!'
+10.times do
+  restaurant = Restaurant.create!(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.street_address,
+    category: %w[chinese italian japanese french belgian].sample
+  )
+  3.times do
+    review = Review.create!(
+      content: Faker::Restaurant.review,
+      rating: rand(1..5),
+      restaurant_id: restaurant.id
+    )
+  end
+  puts "Restaurant #{restaurant.id} is created!"
+end
+puts 'done!'
